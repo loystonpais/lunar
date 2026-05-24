8c30c9f8d3505b5d11c1600429512038e3344183
loystonpais <loyston500@gmail.com>
2026-05-24 14:43:45 +0000

    acme: add dedyn-io provider

--- modules/lunar/acme.nix
+++ modules/lunar/acme.nix
@@ -11,6 +11,54 @@
       };
     };
 
+    provides.dedyn-io = {
+      domain,
+      cert ? {listenHTTP = ":80";},
+    }: let
+      domainName = "${domain}.dedyn.io";
+      accountName = lib.last (lib.splitString "." domainName);
+    in {
+      nixos = {
+        pkgs,
+        config,
+        ...
+      }: let
+        passwordPath = config.sops.secrets."dedyn-io-accounts/${accountName}/password".path;
+      in {
+        security.acme = {
+          acceptTerms = true;
+          certs = {
+            "${domainName}" = lib.mkIf (cert != null) cert;
+          };
+        };
+
+        systemd.services."acme-dns-update-${domainName}" = {
+          enable = true;
+          path = with pkgs; [curl];
+          script = ''
+            for i in $(seq 1 5); do
+              curl -fsS "https://update.dedyn.io/?hostname=${domainName}&password=$(cat ${passwordPath})" && exit 0
+              sleep 3
+            done
+            exit 1
+          '';
+          before = ["acme-${domainName}.service"];
+          serviceConfig = {
+            Type = "oneshot";
+          };
+          wants = ["network-online.target"];
+          after = ["network-online.target"];
+          wantedBy = ["multi-user.target"];
+        };
+
+        sops.secrets."dedyn-io-accounts/${accountName}/password" = {};
+
+        # Let's just disable this for VMs
+        virtualisation.vmVariant.systemd.services."acme-dns-update-${domainName}".enable = lib.mkForce false;
+        virtualisation.vmVariant.systemd.services."acme-${domainName}".enable = lib.mkForce false;
+      };
+    };
+
     provides.freedns-afraid = {
       domainName,
       cert ? {listenHTTP = ":80";},
