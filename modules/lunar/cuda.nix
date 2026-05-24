b590f8ed477776735e1fb3529705a8035e5e1fd3
loystonpais <loyston500@gmail.com>
2026-05-24 14:45:09 +0000

    cuda: prioritize flox cache

--- modules/lunar/cuda.nix
+++ modules/lunar/cuda.nix
@@ -1,8 +1,12 @@
 {den, ...}: {
   lunar.cuda = {
-    nixos = {config, ...}: {
+    nixos = {
+      config,
+      lib,
+      ...
+    }: {
       nix.settings = {
-        substituters = ["https://cache.flox.dev"];
+        substituters = lib.mkBefore ["https://cache.flox.dev"];
         trusted-public-keys = [
           "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
         ];
